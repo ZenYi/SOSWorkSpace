0ac79d50bb0fc017cbddd53a31a95309fdd7321c
ZenYi <461386901@qq.com>
2018-12-25 18:21:38 +0800

    my Adminserver

---
+++ Fangzhen/AdminServer/AdminServer.cpp
@@ -0,0 +1,10 @@
+#include "stdio.h"
+#include "io.h"
+#include <iostream>
+
+int main()
+{
+	//TODO:实现简单的用户管理、组件库管理、文件下载管理、任务管理
+	//用户数据最好能够连接至服务器
+	//
+}
---
+++ Fangzhen/DeviceAgent/DeviceAgent.cpp
@@ -0,0 +1,7 @@
+
+
+int main()
+{
+	//TODO:实现与Adminserver保持连接的程序
+	//承担文件下载的部分
+}
