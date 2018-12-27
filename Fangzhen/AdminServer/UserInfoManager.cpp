bb187b4b095397926e2b908c2d274e0b4348571d
ZenYi <461386901@qq.com>
2018-12-27 17:18:58 +0800

    2018/12/27 17:18

---
+++ Fangzhen/AdminServer/UserInfoManager.cpp
@@ -0,0 +1,24 @@
+#include "AdminServer.h"
+
+template<typename T>
+class UserInfo
+{
+public:
+	UserInfo();
+	~UserInfo();
+
+private:
+
+	int age;
+
+};
+
+template<typename T>
+UserInfo::UserInfo()
+{
+}
+
+template<typename T>
+UserInfo::~UserInfo()
+{
+}
